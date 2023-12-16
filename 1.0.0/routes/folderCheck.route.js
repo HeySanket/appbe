f1d0c9bd8d52150efbea39d68039e3e50e183621
hey.Sanket <golesanket0@gmail.com>
2023-12-16 08:09:52 +0530

    env change

--- 1.0.0/routes/folderCheck.route.js
+++ 1.0.0/routes/folderCheck.route.js
@@ -2,23 +2,32 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
-router.use("/static", express.static("C:/"));
+router.use("/static", express.static(path.join("C:/")));
 
 router.get("/", async (req, res) => {
   console.log(req);
-  try {
-    const data = fs.readdirSync("C:/");
-    console.log(data);
-    res.status(200).send({ data });
-  } catch (error) {
-    res.status(500).send(error);
+  const { name } = req.query;
+  if (name) {
+    const data = fs
+      .readdirSync(path.join(`${process.env.DRIVE}:/`))
+      .filter((val) => {
+        return name == val;
+      });
+    res.sendFile(path.join(`${process.env.DRIVE}:/${data[0]}`));
+  } else {
+    try {
+      const data = fs.readdirSync(path.join(`${process.env.DRIVE}:/`));
+      res.status(200).send({ data });
+    } catch (error) {
+      res.status(500).send(error);
+    }
   }
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const data = fs.readdirSync(`C:/${id}`);
+    const data = fs.readdirSync(`${process.env.DRIVE}:/${id}`);
     res.status(200).send({ data });
   } catch (error) {
     res.status(500).send(error);
@@ -27,11 +36,24 @@ router.get("/:id", async (req, res) => {
 
 router.get("/:firstName/:secondName", async (req, res) => {
   const { firstName, secondName } = req.params;
-  try {
-    const data = fs.readdirSync(`C:/${firstName}/${secondName}`);
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send(error);
+
+  const directories = await fs
+    .readdirSync(
+      path.join(`${process.env.DRIVE}:/${firstName}/${secondName}`),
+      {
+        withFileTypes: true,
+      }
+    )
+    .filter((dirent) => dirent.isDirectory())
+    .map((dir) => dir.name);
+
+  if (directories.length) {
+    res.status(200).send({ data: directories, isDirectory: true });
+  } else {
+    const data = await fs.readdirSync(
+      path.join(`${process.env.DRIVE}:/${firstName}/${secondName}`)
+    );
+    res.status(200).send({ data, isDirectory: false });
   }
 });
 
